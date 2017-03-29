fa626112f418d215a76970be5cb965c0db01f739
Jeremy Guillette <jguillette@fas.harvard.edu>
2017-03-29 09:51:07 -0400

    added date reading, file configuration functions
    The remote announcement json file location is now determined in config.ini, and the js and css files to display the announcement are only loaded if the current date is between the two start and end dates in the json file. Work was done in a mob programming session with @bilbe, @arthurian, and @brandonATG

--- AnnouncementsPlugin.php
+++ AnnouncementsPlugin.php
@@ -9,12 +9,22 @@ class AnnouncementsPlugin extends Omeka_Plugin_AbstractPlugin
     "admin_head",
     // "admin_dashboard"
   );
-
+	function getJsonUrl() {
+		$config = new Zend_Config_Ini(dirname(__FILE__)."/config.ini");
+		return $config->json_url;
+	}
   function hookAdminHead($view){
-		$announcements = json_decode(file_get_contents('http://dighist.fas.harvard.edu/announcement.json'));
-    queue_css_file("announcements");
-    queue_js_file("announcements");
-		queue_js_string("announcements = ".json_encode($announcements).";");
+		$announcements = json_decode(file_get_contents($this->getJsonUrl()));
+		debug($announcements->start);
+		$start = new DateTime($announcements->start);
+		$end = new DateTime($announcements->end);
+		$now = new DateTime('NOW');
+		if ($now > $start && $now < $end) {
+			queue_css_file("announcements");
+	    queue_js_file("announcements");
+			queue_js_string("announcements = ".json_encode($announcements).";");
+		}
+
   }
   // function hookAdminDashboard($view){
   //   echo "<section class='announcement'>This is a test</section>";
