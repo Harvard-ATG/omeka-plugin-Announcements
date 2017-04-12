89be3855f8f20b95c355c476733ac71e68ff72dc
Arthur Barrett <abarrett@fas.harvard.edu>
2017-04-12 12:19:25 -0400

    Refactored plugin php and js.

--- AnnouncementsPlugin.php
+++ AnnouncementsPlugin.php
@@ -1,33 +1,65 @@
 <?php
 /**
  * @package Announcements
+ *
+ * Displays a banner announcement to authenticated users on the admin page.
+ *
+ * The config.ini should contain a "json_url" pointing to a JSON document
+ * that looks something like this:
+ *
+ * {
+ *   text: "This site will be undergoing maintenance from 3pm to 4pm today.",
+ *   start: "2017-04-11T00:00:00Z",
+ *   end: "2017-04-11T23:00:00Z",
+ *   color: "#f9c83d"
+ * }
+ *
+ * Required fields: text, start, end
+ * Optional fields: color
  */
 
 class AnnouncementsPlugin extends Omeka_Plugin_AbstractPlugin
 {
-	protected $_hooks = array(
-    "admin_head",
-    // "admin_dashboard"
+  protected $_hooks = array(
+    "admin_head"
   );
-	function getJsonUrl() {
-		$config = new Zend_Config_Ini(dirname(__FILE__)."/config.ini");
-		return $config->json_url;
-	}
-  function hookAdminHead($view){
-		$announcements = json_decode(file_get_contents($this->getJsonUrl()));
-		debug($announcements->start);
-		$start = new DateTime($announcements->start);
-		$end = new DateTime($announcements->end);
-		$now = new DateTime('NOW');
-		if ($now > $start && $now < $end) {
-			queue_css_file("announcements");
-	    queue_js_file("announcements");
-			queue_js_string("announcements = ".json_encode($announcements).";");
-		}
 
+  function getConfig() {
+    return new Zend_Config_Ini(dirname(__FILE__)."/config.ini", APPLICATION_ENV);
   }
-  // function hookAdminDashboard($view){
-  //   echo "<section class='announcement'>This is a test</section>";
-  // }
+
+  function loadData() {
+    $config = $this->getConfig();
+    $contents = file_get_contents($config->json_url);
+    if ($contents === FALSE) {
+      debug("No content fetched for announcement url: $url");
+			debug("HTTP response headers: ".var_export($http_response_header, 1));
+      return FALSE;
+    }
+
+    $data = json_decode($contents);
+    if (is_null($data)) {
+      debug("Error decoding announcement JSON from string: $contents");
+      return FALSE;
+    }
+    debug("Loaded announcement data: ".var_export($data,1));
+
+    return $data;
+  }
+
+  function hookAdminHead($view){
+    $data = $this->loadData();
+    if ($data === FALSE) {
+      return;
+    }
+
+    $start = new DateTime($data->start);
+    $end = new DateTime($data->end);
+    $now = new DateTime('NOW');
+    if ($now > $start && $now < $end) {
+      queue_css_file("announcements");
+      queue_js_file("announcements");
+      queue_js_string("window.queue_announcement(".json_encode($data, JSON_PRETTY_PRINT).");");
+    }
+  }
 }
-?>
--- views/admin/javascripts/announcements.js
+++ views/admin/javascripts/announcements.js
@@ -1,9 +1,40 @@
-jQuery(document).ready(function() {
-  if ('color' in announcements) {
-    divtag = "<div id='announcement' style='background-color:"+announcements.color+"'>";
-  } else {
-    divtag = "<div id='announcement'>"
+(function($) {
+  /**
+   * Holds the current announcement object.
+   */
+  var announcement = {};
+
+  /**
+   * Renders an announcement item as a div element.
+   */
+  function render(data) {
+    data = data || {};
+    var div = document.createElement('div');
+    div.id = 'announcement';
+    div.innerHTML = data.text || '';
+    if ('color' in data) {
+      div.style = 'background-color: ' + data.color + ';';
+    }
+    return div;
   }
-    jQuery("body").prepend(divtag+announcements.text+"</div>");
-  }
-);
+
+  /**
+   * Displays the announcement when the page is ready.
+   */
+  $(document).ready(function() {
+    var div = render(announcement);
+    $('body').prepend(div);
+  });
+
+  /**
+   * Export function so it's available on the window object.
+   * Intended for use by the plugin hook.
+   */
+  window.queue_announcement = function(data) {
+    announcement = data;
+    if(console && console.log) {
+      console.log("Queued announcement:", announcement);
+    }
+  };
+
+})(jQuery);
